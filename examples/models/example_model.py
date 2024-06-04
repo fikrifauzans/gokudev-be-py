be1feb4ad700d9f9adab4dba7d6cf7aca273df7e
Fikri Fauzan <fikrifauzans.goku@gmail.com>
2024-06-04 19:25:16 +0700

    add migration

--- examples/models/example_model.py
+++ examples/models/example_model.py
@@ -2,6 +2,7 @@ from django.db import models
 from pygments.lexers import get_all_lexers  # type: ignore
 from pygments.styles import get_all_styles  # type: ignore
 
+
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
@@ -9,9 +10,24 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 class ExampleModel(models.Model):
 
+    id = models.AutoField(primary_key=True)
     string = models.CharField(null=True)
-    integer = models.BigIntegerField(null=True)
-    
+    text = models.TextField(null=True)
+    smallint = models.SmallIntegerField(null=True)
+    integer = models.IntegerField(null=True)
+    biginteger = models.BigIntegerField(null=True)
+    boolean = models.BooleanField(null=False, default=False)
+    date = models.DateField(null=True)
+    datetime = models.DateTimeField(null=True)
+    time = models.TimeField(null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    created_by = models.BigIntegerField(null=True)
+    updated_by = models.BigIntegerField(null=True)
+    deleted_by = models.BigIntegerField(null=True)
 
     class Meta:
-        ordering = ["created"]
+        ordering = ["created_by"]
---
+++ examples/serializers/example_serializer.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from examples.models.example_model import ExampleModel, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class ExampleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    string = serializers.CharField(required=False)
+    text = serializers.CharField(required=False)
+    smallint = serializers.IntegerField(required=False)
+    integer = serializers.IntegerField(required=False)
+    biginteger = serializers.IntegerField(required=False)
+    boolean = serializers.BooleanField(required=True)
+    date = serializers.DateField(required=False)
+    datetime = serializers.DateTimeField(required=False)
+    time = serializers.TimeField(required=False)
+
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+    deleted_at = serializers.DateTimeField(required=False)
+
+    created_by = serializers.IntegerField(required=False)
+    updated_by = serializers.IntegerField(required=False)
+    deleted_by = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return ExampleModel.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.string = validated_data.get("string", instance.string)
+        instance.text = validated_data.get("text", instance.text)
+        instance.smallint = validated_data.get("smallint", instance.smallint)
+        instance.integer = validated_data.get("integer", instance.integer)
+        instance.biginteger = validated_data.get("biginteger", instance.biginteger)
+        instance.boolean = validated_data.get("boolean", instance.boolean)
+        instance.date = validated_data.get("date", instance.date)
+        instance.datetime = validated_data.get("datetime", instance.datetime)
+        instance.time = validated_data.get("time", instance.time)
