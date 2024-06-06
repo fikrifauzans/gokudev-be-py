074789027ab8de71935a8fc8bfa50179f9ebef75
Fikri Fauzan <fikrifauzans.goku@gmail.com>
2024-06-06 22:50:25 +0700

    paginator on general repo

--- bedjango/repository.py
+++ bedjango/repository.py
@@ -1,10 +1,47 @@
- 
- 
+from bedjango.dto import DTO
+from django.db import models
+from rest_framework import serializers
+from django.core.paginator import Paginator
+
+
 class GeneralRepository:
-    
-    def __init__(self) -> (None):
-        pass
-    
-   
-    
-    
+
+    _paginator: Paginator
+
+    def __init__(self) -> None:
+        self._paginator = Paginator
+
+    def get_paginator(self, **args) -> Paginator:
+        return self._paginator(**args)
+
+    def paginate_if_has_request_page_and_page_size(
+        self,
+        query_params: DTO,
+        filtered_queryset: models.Model,
+        serializer: serializers.Serializer,
+    ):
+        if query_params._raw_request.query_params.get("page_size") == None:
+            return {
+                "total": len(filtered_queryset.values()),
+                "data": filtered_queryset.values(),
+            }
+        else:
+
+            paginator = self._paginator(
+                filtered_queryset,
+                query_params._raw_request.query_params.get("page_size", 10),
+            )
+            page = paginator.get_page(
+                query_params._raw_request.query_params.get("page", 1)
+            )
+
+            serializer = serializer(page.object_list, many=True)
+            return {
+                "total": paginator.count,
+                "total_pages": paginator.num_pages,  # Added total pages
+                "next": page.next_page_number() if page.has_next() else None,
+                "previous": (
+                    page.previous_page_number() if page.has_previous() else None
+                ),
+                "data": serializer.data,
+            }
--- examples/repositories/example_repository.py
+++ examples/repositories/example_repository.py
@@ -1,32 +1,22 @@
 from examples.dto.example_dto import ExampleDTO
 from examples.models.example_model import ExampleModel as Model
 from bedjango.repository import GeneralRepository
-from examples.serializers.example_serializer import ExampleSerializer
-from django.core.paginator import Paginator
+from examples.serializers.example_serializer import ExampleSerializer as Serializer
 
 
 class ExampleRepository(GeneralRepository):
+    serializer: Serializer
+    model: Model
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.model = Model
+        self.serializer = Serializer
+        super().__init__(*args, **kwargs)
 
     def get(self, query_params: ExampleDTO) -> any:
-     
-        filtered_queryset = Model.objects.filter(**query_params._query_set)
 
-        if query_params._raw_request.query_params.get("page_size") == None:
-            return {
-                "total": len(filtered_queryset.values()),
-                "data": filtered_queryset.values(),
-            }
+        filtered_queryset = self.model.objects.filter(**query_params._query_set)
 
-        paginator = Paginator(
-            filtered_queryset, query_params._raw_request.query_params.get("page_size", 10)
+        return self.paginate_if_has_request_page_and_page_size(
+            query_params, filtered_queryset, self.serializer
         )
-        page = paginator.get_page(query_params._raw_request.query_params.get("page", 1))
-
-        serializer = ExampleSerializer(page.object_list, many=True)
-        return {
-            "total": paginator.count,
-            "total_pages": paginator.num_pages,  # Added total pages
-            "next": page.next_page_number() if page.has_next() else None,
-            "previous": page.previous_page_number() if page.has_previous() else None,
-            "data": serializer.data,
-        }
